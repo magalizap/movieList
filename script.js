629a8d5ea456379762fcdf416ec7cf7901b883c5
Magali Sol <magalisol2307@gmail.com>
2022-08-01 16:21:15 -0300

    modificamos los estilos de css y agregamos funciones en js

--- script.js
+++ script.js
@@ -17,9 +17,13 @@ const formPelis = document.getElementById("formPelis")
 const mostrar = document.getElementById("mostrar")
 const verListado = document.getElementById("verListado")
 const listaVacia = document.getElementById("listaVacia")
+const lista = document.getElementById("lista")
 
 
 //creamos un evento de tipo formulario para guardar los valores que me ingrese el usuario, en mismo evento mostramos y eliminamos las cards para una mejor experiencia de usuario
+
+
+
 formPelis.addEventListener("submit", (e) =>{
     e.preventDefault()
 
@@ -41,6 +45,85 @@ formPelis.addEventListener("submit", (e) =>{
     console.log(movies)
 
     const datosStorage = JSON.parse(localStorage.getItem("Movies"))
+
+    mostrar.innerHTML = ""
+    
+    datosStorage.forEach((movie, indice) => {
+
+        mostrar.innerHTML += `
+        <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em">
+            <div class="card-body">
+            <h5 class="card-title text-dark" >${movie.nombrePeli}</h5>
+            <h6 class="card-text text-dark">Categoría: ${movie.categoria}</h6>
+            <h6 class="card-text text-dark">Ver en: ${movie.plataforma}</h6>
+            <a href="#" class="btn btn-secondary">Eliminar</a>
+            </div>
+        </div>
+        
+        `
+        if(movies.length === 0){
+            listaVacia.classList.remove("empty")
+                
+        }else{
+            listaVacia.classList.add("empty")
+        }
+
+
+    })
+        // eliminamos la tarea de la lista
+    datosStorage.forEach((movie, indice) => {
+
+        const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
+        
+        eliminar.addEventListener("click", ()=>{
+
+            document.getElementById(`pelis${indice}`).remove()
+            movies.splice(indice,1)
+            localStorage.setItem("Movies", JSON.stringify(movies))
+            console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
+
+            console.log(movies)
+
+            if(movies.length === 0){
+                listaVacia.classList.remove("empty")
+                    
+            }else{
+                listaVacia.classList.add("empty")
+            }
+    
+            
+    
+        })
+    
+    
+    })
+
+
+    
+})
+
+
+// OPERADOR SPREAD 
+
+/*const guardarStorage = structuredClone(datosStorage)
+console.log(guardarStorage)
+
+const formPelis2 = structuredClone({
+    formPelis,
+    
+        // eliminamos la tarea de la lista
+
+    
+})*/
+
+
+
+
+
+lista.addEventListener("click", () =>{
+
+
+    const datosStorage = JSON.parse(localStorage.getItem("Movies"))
     datosStorage.forEach((movie, indice) => {
         mostrar.innerHTML += `
         <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em">
@@ -54,46 +137,29 @@ formPelis.addEventListener("submit", (e) =>{
         
         `
 
-
-
     })
-        // eliminamos la tarea de la lista
-    datosStorage.forEach((movie, indice) =>{
-        const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
-        
-        eliminar.addEventListener("click", ()=>{
-            document.getElementById(`pelis${indice}`).remove()
-            movies.splice(indice,1)
-            localStorage.setItem("Movies", JSON.stringify(movies))
-            console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
-
-           console.log(movies)
-           
-            if(movies.length == 0){
-                listaVacia.classList.remove("empty")
-                
-            }else{
-                listaVacia.classList.add("empty")
-            }
-            
-
-        })
-
-
-    })
-
-
-
-    
 })
-
-
-
 
 verListado.addEventListener("click", () =>{ // Descifrar cómo mostrar solo las cards que aparecen filtradas por categoría
 
     const filtrado = document.getElementById("filtrado").value
     movies.filter(filtrado => filtrado.value)
+
+    const datosStorage = JSON.parse(localStorage.getItem("Movies"))
+    datosStorage.filter((movie, indice) => {
+        filtrado.innerHTML +=  `
+        <div id="pelis${indice.value}" class="card" style="width: 18rem; margin: 1em">
+            <div class="card-body">
+            <h5 class="card-title text-dark" >${movie.nombrePeli}</h5>
+            <h6 class="card-text text-dark">Categoría: ${movie.categoria}</h6>
+            <h6 class="card-text text-dark">Ver en: ${movie.plataforma}</h6>
+            <a href="#" class="btn btn-secondary">Eliminar</a>
+            </div>
+        </div>
+        
+        `
+
+    })
     
     console.log(filtrado) 
 
@@ -111,4 +177,12 @@ verListado.addEventListener("click", () =>{ // Descifrar cómo mostrar solo las 
  *  - Agregar imagenes a las cards => no se que película puede elegir el usuario :(
  *  - crear un filtrado por categorías para que, de haber muchas en la lista, el usuario pueda encontrar la que desee más facilmente
  *  - FALTA AGREGAR CONDICIONALES!!!
+ */
+
+
+
+
+/**
+ *     
+ *     
  */
