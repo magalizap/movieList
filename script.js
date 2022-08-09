cc2f5b358ac571d6d18186eb0c41c9ca22cd37fc
Magali Sol <magalisol2307@gmail.com>
2022-08-08 23:45:17 -0300

    agregamos librerias y mejoramos el dinamismo de los botones

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Creamos la clase Movies
-class Movies{
+class Movie{
     constructor(nombrePeli, categoria, plataforma){
         this.nombrePeli = nombrePeli
         this.categoria = categoria
@@ -8,20 +8,17 @@ class Movies{
     }
 }
 
-
-const movies = []
-
+const movies = JSON.parse(localStorage.getItem("movies")) ?? []
 
 // Llamamos a los id 
 const formPelis = document.getElementById("formPelis")
 const mostrar = document.getElementById("mostrar")
-const verListado = document.getElementById("verListado")
+const botonVerPelis = document.getElementById("botonVerPelis")
 const listaVacia = document.getElementById("listaVacia")
-const lista = document.getElementById("lista")
+const mostrarFiltrado = document.getElementById("mostrarFiltrado")
 
 
 //creamos un evento de tipo formulario para guardar los valores que me ingrese el usuario, en mismo evento mostramos y eliminamos las cards para una mejor experiencia de usuario
-
 
 
 formPelis.addEventListener("submit", (e) =>{
@@ -31,28 +28,41 @@ formPelis.addEventListener("submit", (e) =>{
     const categoria = document.getElementById("categoria").value
     const plataforma = document.getElementById("plataforma").value
     //creamos el objeto movie
-    const movie = new Movies (nombrePeli,  categoria, plataforma)
+    const movie = new Movie (nombrePeli,  categoria, plataforma)
     // agregamos los datos al array 
     movies.push(movie)
     // guardamos en el localSotage
-    localStorage.setItem("Movies", JSON.stringify(movies))
+    localStorage.setItem("movies", JSON.stringify(movies))
 
     // limpiar form
     formPelis.reset()
 
 
     // Mostramos las peliculas agregadas a la lista
-    console.table(movies)
+    console.log(movies)
 
     mostrarLista(movies)
 
+
+    Toastify({
+        text: "Agregaste una nueva peli a la lista",
+        duration: 1000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to bottom right, rgba(2, 36, 239, 1.0), rgba(182, 100, 27, 1.0))",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
 }) 
 
 
  
 const mostrarLista = (movies) =>{
     mostrar.innerHTML = ""
-    const datosStorage = JSON.parse(localStorage.getItem("Movies")) ?? []   //operador nullish
+    const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []   //operador nullish
 
 
     datosStorage.forEach((movie, indice) => {
@@ -61,12 +71,12 @@ const mostrarLista = (movies) =>{
 
         mostrar.innerHTML += `
         <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em">
-            <div class="card-body">
-            <h5 class="card-title text-dark" >${nombrePeli}</h5>
-            <h6 class="card-text text-dark">Categoría: ${categoria}</h6>
-            <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
-            <a href="#" class="btn btn-secondary">Eliminar</a>
-            </div>
+        <div class="card-body">
+        <h5 class="card-title text-dark" >${nombrePeli}</h5>
+        <h6 class="card-text text-dark">Categoría: ${categoria}</h6>
+        <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
+        <a href="#" class="btn btn-secondary">Eliminar</a>
+        </div>
         </div>
         
         `
@@ -78,17 +88,15 @@ const mostrarLista = (movies) =>{
 
         // eliminamos la tarea de la lista
         const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
-
-
         eliminar.addEventListener("click", ()=> {
 
             document.getElementById(`pelis${indice}`).remove()
-            movies.splice(movies.indexOf(indice), 1)
-
-            localStorage.setItem("Movies", JSON.stringify(movies))
+            //movies.splice(movies.indexOf(indice), 1)
+            movies.splice(indice,1)
+            localStorage.setItem("movies", JSON.stringify(movies))
             console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
 
-            console.table(movies)
+            console.log(movies)
 
 
 
@@ -97,39 +105,115 @@ const mostrarLista = (movies) =>{
 
         })
     })
+
+
+    
+} 
+
+
+
+botonVerPelis.addEventListener("click", () =>{
+    mostrar.innerHTML = ""
+    const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []   //operador nullish
+
+
+    datosStorage.forEach((movie, indice) => {
+
+        let {nombrePeli, categoria, plataforma} = movie //Desestructuración de código
+        
+        mostrar.innerHTML += `
+        <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em  "> 
+        <div class="card-body">
+        <h5 class="card-title text-dark" >${nombrePeli}</h5>
+        <h6 class="card-text text-dark">Categoría: ${categoria}</h6>
+        <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
+        <a href="#" class="btn btn-secondary">Eliminar</a>
+        </div>
+        </div>
+        
+        `
+        movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
+
+    })
+
+    datosStorage.forEach((movie, indice)=>{
+
+        // eliminamos la tarea de la lista
+        const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
+
+        eliminar.addEventListener("click", ()=> {
+
+            document.getElementById(`pelis${indice}`).remove()
+            //movies.splice(movies.indexOf(indice), 1)
+            movies.splice(indice,1)
+            localStorage.setItem("movies", JSON.stringify(movies))
+            console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
+
+            console.log(movies)
+
+
+
+            movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
+
+
+        })
+    })
+})
+
+
+
+
+function seleccionarCategoria(){
+    const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []  
+
+    datosStorage.forEach((movie, indice) =>{
+        
+        let filtrado = document.getElementById("filtrado")
+        let categoriaElegida = filtrado.value
+        let {nombrePeli, plataforma} = movie
+        
+        mostrarFiltrado.innerHTML = `
+        <div id="pelis${indice}"  class="card" style="width: 18rem; margin: 1em  "> 
+        <div class="card-body">
+        <h5 class="card-title text-dark" >${nombrePeli}</h5>
+        <h6 class="card-text text-dark">Categoría: ${categoriaElegida}</h6>
+        <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
+        <a href="#" class="btn btn-secondary">Eliminar</a>
+        </div>
+        </div>
+        
+        `
+    })
+    
 }
 
 
 
 
 
-verListado.addEventListener("click", () =>{ // Descifrar cómo mostrar solo las cards que aparecen filtradas por categoría
 
-    mostrar.innerHTML = ""
-    const filtrado = document.getElementById("filtrado").value
-    movies.filter(filtrado => filtrado.value)
 
-    const datosStorage = JSON.parse(localStorage.getItem("Movies")) ?? []
-    datosStorage.filter((movie, indice) => {
 
-        let {nombrePeli,  categoria, plataforma} = movie //Desestructuración de código
-        mostrar.innerHTML +=  `
-        <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em">
-            <div class="card-body">
-            <h5 class="card-title text-dark" >${nombrePeli}</h5>
-            <h6 class="card-text text-dark">Categoría: ${categoria}</h6>
-            <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
-            <a href="#" class="btn btn-secondary">Eliminar</a>
-            </div>
-        </div>
-        
-        `
 
-    })
-    
-    console.log(filtrado) 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
