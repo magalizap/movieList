f937f8a9af5e752bb5fa72e63bedca1adcff13a8
Magali Sol <magalisol2307@gmail.com>
2022-08-15 19:25:55 -0300

    incorporamos la api tmdb

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const formPelis = document.getElementById("formPelis")
 const mostrar = document.getElementById("mostrar")
 const botonVerPelis = document.getElementById("botonVerPelis")
 const listaVacia = document.getElementById("listaVacia")
-const mostrarFiltrado = document.getElementById("mostrarFiltrado")
+const filtrado = document.getElementById("filtrado")
 
 
 //creamos un evento de tipo formulario para guardar los valores que me ingrese el usuario, en mismo evento mostramos y eliminamos las cards para una mejor experiencia de usuario
@@ -57,16 +57,88 @@ formPelis.addEventListener("submit", (e) =>{
         },
         onClick: function(){} // Callback after click
       }).showToast();
+
+      
 }) 
 
 
- 
+
+
+
+
+
+
 const mostrarLista = (movies) =>{
+    botonVerPelis.addEventListener("click", () =>{
+    
+        mostrar.innerHTML = ""
+        //const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []   //operador nullish
+    
+    
+        movies.forEach((movie, indice) => {
+    
+            let {nombrePeli, categoria, plataforma} = movie //Desestructuración de código
+    
+            mostrar.innerHTML += `
+            <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em">
+            <div class="card-body">
+            <h5 class="card-title text-dark" >${nombrePeli}</h5>
+            <h6 class="card-text text-dark">Categoría: ${categoria}</h6>
+            <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
+            <a href="#" class="btn btn-secondary">Eliminar</a>
+            </div>
+            </div>
+    
+    
+            `
+            movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
+    
+        })
+        movies.forEach((movie, indice)=>{
+    
+            // eliminamos la tarea de la lista
+            const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
+            eliminar.addEventListener("click", () => {
+    
+                document.getElementById(`pelis${indice}`).remove()
+                const moviesEliminadas = movies.splice(movies.indexOf(indice, 1))
+                //movies.splice(indice,1)
+
+
+                localStorage.setItem("movies", JSON.stringify(movies))
+                console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
+    
+
+    
+    
+    
+                movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
+    
+    
+            })
+        })
+    })
+}
+
+
+
+botonVerPelis.addEventListener("click", () =>{
+    mostrarLista(movies)
+
+})
+
+
+
+
+
+filtrado.addEventListener("change", () =>{
+    mostrarLista(movies) 
     mostrar.innerHTML = ""
-    const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []   //operador nullish
 
+    //const pelisFiltradas = movies.filter(categoria => categoria.value == filtrado.value )
+    const pelisFiltradas = movies.filter(peli => peli.categoria == filtrado.value )
 
-    datosStorage.forEach((movie, indice) => {
+    pelisFiltradas.forEach((movie, indice) => {
 
         let {nombrePeli, categoria, plataforma} = movie //Desestructuración de código
 
@@ -84,69 +156,16 @@ const mostrarLista = (movies) =>{
         movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
 
     })
-       
-    datosStorage.forEach((movie, indice)=>{
-
-        // eliminamos la tarea de la lista
-        const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
-        eliminar.addEventListener("click", ()=> {
-
-            document.getElementById(`pelis${indice}`).remove()
-            //movies.splice(movies.indexOf(indice), 1)
-            movies.splice(indice,1)
-            localStorage.setItem("movies", JSON.stringify(movies))
-            console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
-
-            console.log(movies)
-
-
-
-            movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
-
-
-        })
-    })
-
-
     
-} 
-
-
-
-botonVerPelis.addEventListener("click", () =>{
-    mostrar.innerHTML = ""
-    const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []   //operador nullish
-
-
-    datosStorage.forEach((movie, indice) => {
-
-        let {nombrePeli, categoria, plataforma} = movie //Desestructuración de código
-        
-        mostrar.innerHTML += `
-        <div id="pelis${indice}" class="card" style="width: 18rem; margin: 1em  "> 
-        <div class="card-body">
-        <h5 class="card-title text-dark" >${nombrePeli}</h5>
-        <h6 class="card-text text-dark">Categoría: ${categoria}</h6>
-        <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
-        <a href="#" class="btn btn-secondary">Eliminar</a>
-        </div>
-        </div>
-        
-        `
-        movies.length === 0 ?  listaVacia.classList.remove("empty") : listaVacia.classList.add("empty")
-
-    })
-
-    datosStorage.forEach((movie, indice)=>{
+    pelisFiltradas.forEach((movie, indice)=>{
 
         // eliminamos la tarea de la lista
         const eliminar = document.getElementById(`pelis${indice}`).lastElementChild.lastElementChild
-
         eliminar.addEventListener("click", ()=> {
 
             document.getElementById(`pelis${indice}`).remove()
-            //movies.splice(movies.indexOf(indice), 1)
-            movies.splice(indice,1)
+            const moviesEliminadas = movies.splice(movies.indexOf(indice, 1))
+            //movies.splice(indice,1)
             localStorage.setItem("movies", JSON.stringify(movies))
             console.log(`${movie.nombrePeli} Fue eliminada de la lista`)
 
@@ -164,41 +183,50 @@ botonVerPelis.addEventListener("click", () =>{
 
 
 
-function seleccionarCategoria(){
-    const datosStorage = JSON.parse(localStorage.getItem("movies")) ?? []  
 
-    datosStorage.forEach((movie, indice) =>{
+// CREAMOS NUESTRO CATALOGO DE PELICULAS
 
-        let filtrado = document.getElementById("filtrado")
-        let categoriaElegida = filtrado.value
-        let {nombrePeli, plataforma} = movie
-        
-        mostrarFiltrado.innerHTML = `
-        <div id="pelis${indice}"  class="card" style="width: 18rem; margin: 1em  "> 
-        <div class="card-body">
-        <h5 class="card-title text-dark" >${nombrePeli}</h5>
-        <h6 class="card-text text-dark">Categoría: ${categoriaElegida}</h6>
-        <h6 class="card-text text-dark">Ver en: ${plataforma}</h6>
-        <a href="#" class="btn btn-secondary">Eliminar</a>
-        </div>
-        </div>
-        
-        `
-    })
+
+const API_KEY = "963613df74224797f8231f9800d2a1ca"
+const pelis = document.getElementById("pelis")
+const catalogo = document.getElementById("catalogo")
+const verCatalogo = document.getElementById("verCatalogo")
+
+
+
+
+verCatalogo.addEventListener("click", () =>{
+
+    getMovies()
+    function getMovies(){
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`)
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data.results)
+            catalogoMovies(data.results)
+        })
+    }
     
-}
-
-
-
-
-
-
-
-
-
-
-
-
+    function catalogoMovies (data){
+        catalogo.innerHTML = ""
+        data.forEach(movie => {
+            //.then(data => console.log(data))
+            let {poster_path, original_title, vote_average, overview} = movie
+            pelis.innerHTML += `
+            <div class="cards">
+                <img class="img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${original_title}">
+                <h3>${original_title}</h3>
+                <span class="puntuacion">${vote_average}</span>
+                <p>${overview}</p>
+            </div>
+            
+            `
+            //console.log(original_title, overview, poster_path)
+            
+            catalogo.appendChild(pelis)
+        })
+    }
+})
 
 
 
@@ -221,13 +249,13 @@ function seleccionarCategoria(){
 /**
  * Cosas para resolver:
  * 
- *  - el boton de mostrar listado me devuelve la peli nueva sumada a las que ya estaban a la vista, me repite cards
- *  - si activo el boton sumar peli para que me muestre las cards también, la función eliminar no funciona 
+ *  - el boton de mostrar listado me devuelve la peli nueva sumada a las que ya estaban a la vista, me repite cards ✔️
+ *  - si activo el boton sumar peli para que me muestre las cards también, la función eliminar no funciona ✔️
  *  - mejorar los estilos css, eliminar las tarjetas bootstrap por tarjetas de css
  *  - Crear un formulario de login, para que más de un usuario tenga su cuenta con sus peliculas a elección
  *  - Agregar imagenes a las cards => no se que película puede elegir el usuario :(
- *  - crear un filtrado por categorías para que, de haber muchas en la lista, el usuario pueda encontrar la que desee más facilmente
- *  - FALTA AGREGAR CONDICIONALES!!!
+ *  - crear un filtrado por categorías para que, de haber muchas en la lista, el usuario pueda encontrar la que desee más facilmente ✔️
+ *  - FALTA AGREGAR CONDICIONALES!!! ✔️
  */
 
 
